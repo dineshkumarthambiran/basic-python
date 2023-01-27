80fda9c1d846bff663aec33e6864d47fe64c8027
dineshkumarthambiran <dineshkumarthambiran96@gmail.com>
2023-01-27 14:06:11 +0530

    json

--- rockpaperscissor.py
+++ rockpaperscissor.py
@@ -1,19 +1,23 @@
 import random
 
+print("hello world")
+
 print("Lets play the Game ! Rock Paper and Scissors ")
 
+user= input("enter your choice \n 'r' for rock \n 'p' for paper \n 's' for scissors")
+print(user)
+computer = random.choice(['r','p','s'])
+print(computer)
+
 def is_win(player,opponent):
-    user= input("enter your choice \n 'r' for rock \n 'p' for paper \n 's' for scissors")
-    print(user)
-    computer = random.choice(['r','p','s'])
-    print(computer)
+    
 
     if (player=='r' and opponent=='s') or (player=='s' and opponent=='p') or (player=='r' and opponent=='p'):
         return "user has won"
         
     return "computer has won"
     
-
+print(is_win(user,computer))
 # def play():
 #     if is_win(user,computer) is True:
 #         print( "user has won")
@@ -22,7 +26,7 @@ def is_win(player,opponent):
     
 
 
-is_win(user,computer)
+
 
 # def play() :
 #     print(is_win(user,computer))
